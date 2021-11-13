e10f64ca2f94721d6546736fb1e11dfc01e14089
leda84 <laura.dejesus929@gmail.com>
2021-11-13 10:57:20 -0600

    connected local db, added flash msg

--- hero_project/authentication/routes.py
+++ hero_project/authentication/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 import flask_wtf
 from hero_project.forms import UserLoginForm
+from hero_project.models import db,User
+
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
@@ -13,9 +15,12 @@ def signup():
             password = form.password.data
             print(email,password)
 
-            # TODO- ADD USER TO DATABASE
+            # creating/adding user to database
+            user = User(email, password = password)
+            db.session.add(user)
+            db.session.commit()
             
-            flash(f'You have successfully created a user account for {email}. \nWelcome to The Hero Project', "user-created")
+            flash(f'You have successfully created a user account for {email}. \nWelcome to The Hero Project!', "user-created")
             return redirect(url_for('site.home'))   #maybe change redirect to profile(maybe even pop up option to choose)
     except:
         raise Exception('Invalid Form Data: Please check your info...')
